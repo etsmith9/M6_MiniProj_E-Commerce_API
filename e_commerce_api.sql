32931ffc0014ae11ace6edcaa30a286ae4ff77f1
Emily Smith <etsmith9@gmail.com>
2025-01-24 10:45:54 -0500

    Update e_commerce_api.sql

--- e_commerce_api.sql
+++ e_commerce_api.sql
@@ -5,16 +5,16 @@ USE e_commerce_api;
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    email VARCHAR(320) DEFAULT NULL,
-    phone VARCHAR(15) DEFAULT NULL
+    email VARCHAR(320),
+    phone VARCHAR(15)
 );
 
 CREATE TABLE customer_accounts (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    user_id INT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE products (
@@ -27,15 +27,24 @@ CREATE TABLE products (
 CREATE TABLE orders (
     id INT AUTO_INCREMENT PRIMARY KEY,
     date VARCHAR(255) NOT NULL,
-    user_id INT NOT NULL,
+    user_id INT,
     total_price FLOAT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE order_product (
     order_id INT NOT NULL,
     product_id INT NOT NULL,
     PRIMARY KEY (order_id, product_id),
+    FOREIGN KEY (order_id) REFERENCES orders(id) ON DELETE CASCADE,
+    FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE
+);
+
+CREATE TABLE order_items (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT,
+    product_id INT,
+    quantity INT NOT NULL,
     FOREIGN KEY (order_id) REFERENCES orders(id) ON DELETE CASCADE,
     FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE
 );
